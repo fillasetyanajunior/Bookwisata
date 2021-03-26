cbba96964badb584893bc518f336e50d04b7e0f9
fillajunior <mailto:pantailovina@gmail.com>
2021-03-26 08:09:04 +0800

    Update ukuran gambar

--- resources/views/home/utama.blade.php
+++ resources/views/home/utama.blade.php
@@ -73,7 +73,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('mobil/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('mobil/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -179,7 +179,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('paket/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('paket/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -234,7 +234,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('kapal/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('kapal/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -289,7 +289,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('hotel/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('hotel/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -344,7 +344,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('guide/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('guide/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -399,7 +399,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('kuliner/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('kuliner/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -454,7 +454,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('destinasi/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('destinasi/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
@@ -509,7 +509,7 @@
                     <div class="single-place mb-30">
                         <div class="place-img">
                             @foreach ($foto as $foto)
-                            <img src="{{asset('pusat/' . $foto->foto)}}" alt="">
+                            <img src="{{asset('pusat/' . $foto->foto)}}" alt="" height="200px">
                             @endforeach
                         </div>
                         <div class="place-cap">
