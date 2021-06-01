45909f9159906a887e4266ddb7d616a234c9e74f
fillajunior <pantailovina@gmail.com>
2021-06-01 22:29:59 +0800

    update 01/06/21

--- resources/views/home/boardingpass.blade.php
+++ resources/views/home/boardingpass.blade.php
@@ -71,7 +71,7 @@
                 <tbody>
                     <tr>
                         <th scope="row">Harga</th>
-                        <td>{{'Rp. '.strrev(implode('.',str_split(strrev(strval($riwayat->harga)),3)))}}&nbsp;X&nbsp;{{$riwayat->hari}}</td>
+                        <td>{{'Rp. '.strrev(implode('.',str_split(strrev(strval($riwayat->harga)),3)))}}&nbsp;X&nbsp;{{$riwayat->hari}}&nbsp;Hari</td>
                     </tr>
                     <tr>
                         <th scope="row">PPN %</th>
